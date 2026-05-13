4b00686a762b22a8d9ac3d1dc905e5d23cfced69
Nour Shobier <nour.eldin.shobier@gmail.com>
2026-05-13 16:18:52 +0300

    chore(release): v0.5.1

--- dist/index.mjs
+++ dist/index.mjs
@@ -566,7 +566,7 @@ async function removeStaleSourceDirs(repoRoot, config) {
 }
 //#endregion
 //#region src/lib/version.ts
-const VERSION = "0.5.0";
+const VERSION = "0.5.1";
 //#endregion
 //#region src/commands/init.ts
 const WELL_KNOWN_TOOLS = [
